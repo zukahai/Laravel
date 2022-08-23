c7ed2717500e77240525da513c69d7d0a2d847e5
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-08-24 01:41:21 +0700

    Config datatable

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $user = new User();
+    $allUser = $user::all();
+    dd($allUser);
     return view('welcome');
 });
