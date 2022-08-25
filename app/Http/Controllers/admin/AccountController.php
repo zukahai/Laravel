35ab878313357b2535a668ebfa99ce6b4f5bb86d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-08-26 00:04:06 +0700

    Add validation

--- app/Http/Controllers/admin/AccountController.php
+++ app/Http/Controllers/admin/AccountController.php
@@ -39,12 +39,15 @@ class AccountController extends Controller
     public function add(Request $request) {
         $request->validate(
             [
-                'username' => 'required',
-                'password' => 'required|',
+                'username' => 'required|unique:accounts|max:15',
+                'password' => 'required|min:6',
             ],
             [
                 'username.required' => 'Vui lòng nhập tên đăng nhập',
+                'username.unique' => 'Tên tài khoản đã được sử dụng',
+                'username.max' => 'Tên tài khoản không được quá 15 ký tự',
                 'password.required' => 'Vui lòng nhập mật khẩu',
+                'password.min' => 'Mật khẩu phải 6 ký tự',
             ]
         );
         $account = new Account();
